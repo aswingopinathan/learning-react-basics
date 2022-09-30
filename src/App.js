320e43a9b734ee0049d6833e5aa025e2981ec619
Aswin Gopinathan <aswingopinathan@gmail.com>
2022-09-30 18:16:39 +0530

    props and function component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css'
+import Header from './Component/Header' 
 function App() {
   const data1="Aswin";
   return (
     <div>
-      <h1 className='hello' style={{color:'red'}} >Hello World</h1>
+      <Header data={data1}/>
       <p>This is sample description in p tag.</p>
       <p>The name is {data1}</p>
     </div>
   );
 }
-
+ 
 export default App;
+
+
  
---
+++ src/Component/Header.js
@@ -0,0 +1,8 @@
+
+function Header(props){
+    return(
+      <h1 className='hello' style={{color:'red'}} >{props.data} says Hello World</h1>
+    )
+  }
+
+  export default Header;
