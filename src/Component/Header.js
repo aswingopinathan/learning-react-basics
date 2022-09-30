9f061d9dde672a483ee922fbd85866bc230e7071
Aswin Gopinathan <aswingopinathan@gmail.com>
2022-09-30 18:30:14 +0530

    class component

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,8 +1,11 @@
+import {Component} from 'react'
 
-function Header(props){
-    return(
-      <h1 className='hello' style={{color:'red'}} >{props.data} says Hello World</h1>
-    )
+class Header extends Component(){
+    render(){
+        return(
+            <h1 className='hello' style={{color:'red'}} >{this.props.data} says Hello World</h1>
+          )
+    }
   }
 
   export default Header;
